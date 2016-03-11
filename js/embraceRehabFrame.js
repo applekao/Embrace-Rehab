fafa27f224abd5994680bc5642fb557d88525f33
Rubinhuang9239 <huang9239@icloud.com>
2016-03-11 03:32:30 -0500

    update frame

--- js/embraceRehabFrame.js
+++ js/embraceRehabFrame.js
@@ -1,29 +1,28 @@
 var anime = {};
 
 window.addEventListener("load",function(){
-
 	console.log("frame_loaded!");
 	anime.init();
-
 });
 
 
 anime.init = function(){
 	var horizontalPanels = document.getElementsByClassName("panel");
 
-	for(i = 0; i < horizontalPanels.length; i++){
-		var currentDom = horizontalPanels[i];
-		currentDom.addEventListener("click",function(){
-		anime.switchToPanel(currentDom)});
-		console.log(currentDom);
+	var panelToggleBtn = document.getElementsByClassName("panelToggle");
+	for(i = 0; i < panelToggleBtn.length; i++){
+		var currentDom = panelToggleBtn[i];
+		currentDom.addEventListener("click",function(e){
+		anime.switchToPanel(e.target);
+		$(".panelToggle").removeClass("panelToggle_selected");
+		e.target.className += " panelToggle_selected";
+		});
 	}
 }
 
 anime.switchToPanel = function(aim){
-console.log(aim.attributes.switchAim);
 document.getElementById("mainScroll").style.webkitTransform = 
-"translate(" + (aim.childElementCount * (-50)) + "%)";
-
+"translate(" + (parseInt(aim.attributes.switchAim.value) * (-50)) + "%)";
 }
 
 
